927693523626e720b8a1c55289b53c90546628ab
Alessandro Rubini <rubini@gnudd.com>
2015-11-13 07:01:47 +0100

    sw/empty: toggle led
    Signed-off-by: Alessandro Rubini <rubini@gnudd.com>

--- sw/empty.c
+++ sw/empty.c
@@ -3,4 +3,17 @@
 void main(void)
 {
 	puts("Here I am, I do nothing\n");
+
+	/* flash PD5 -- no pwm by now */
+#define BIT (1 << 5)
+	regs[REG_DDRD]  |= BIT;
+	while (1) {
+
+
+		/* copy PD2 to PD5 */
+		if (regs[REG_PIND] & 0x04)
+			regs[REG_PORTD] &= ~BIT;
+		else
+			regs[REG_PORTD] |= BIT;
+	}
 }
